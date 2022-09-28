63419ebf2fe951bf410834103f0300c68a0bb53b
unknown <fobas0405@gmail.com>
2022-09-28 21:42:30 +0300

    first commit

---
+++ uzduotisNr2.py
@@ -0,0 +1,24 @@
+
+
+
+
+luckyNumber = input("Enter seven numbers: ")
+
+
+
+
+my_int = 13579
+
+number_list = [int(x) for x in str(luckyNumber)]
+calculationOne = number_list[1] * number_list[3] * number_list[4]
+calculationTwo = number_list[2] + number_list[3] + number_list[4]
+
+
+print(calculationOne)  
+print(calculationTwo)
+
+
+
+
+
+
